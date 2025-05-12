a0b8c274bce034d7bf0076db744ebe7f7ab6438c
Ivanka <IvNagornyuk@gmail.com>
2025-05-13 02:03:51 +0300

    add function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,30 @@
-'use strict';
+"use strict";
+
+function calculateNewRate(winner, loser) {
+  let newRate = 0;
+  if (winner < 0 || loser < 0) {
+    return "Рейтинг не може бути менше 0";
+  } else {
+    if (winner === 0) {
+      newRate = loser.toFixed(1);
+      // return `Рейтинг переможця ${newRate}`;
+    } else if (winner < loser) {
+      newRate = (loser - winner + 5) / 3+winner;
+      // return `Рейтинг переможця ${newRate.toFixed(1)}`;
+    } else {
+      const diff = winner - loser;
+      if (diff >= 0 && diff <= 2) {
+        newRate = winner + 2;
+        // return `Рейтинг переможця ${newRate.toFixed(1)}`;
+      } else if (diff > 2 && diff < 20) {
+        newRate = winner + 1;
+        // return `Рейтинг переможця ${newRate.toFixed(1)}`;
+      } else if (diff >= 20) {
+        newRate = winner;
+      }
+    }
+    return `Рейтинг переможця ${newRate.toFixed(1)}`;
+  }
+}
+
+console.log(calculateNewRate(8.456, 5.23563));
