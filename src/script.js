8343073995dbfa12f59064c355974b116efdd57f
Ivanka <IvNagornyuk@gmail.com>
2025-05-23 00:18:57 +0300

    add Number

--- src/script.js
+++ src/script.js
@@ -7,13 +7,16 @@
  */
 
 function calculateNewRate(winner, loser) {
+  winner = Number(winner);
+  loser = Number(loser);
+
   if (winner < 0 || loser < 0) return -2;
   if (winner === 0) return loser;
 
   const diff = winner - loser;
 
   if (Number.isNaN(diff)) return -1;
-  if (diff < 0) return (loser - winner + 5) / 3 + winner;
+  if (diff < 0) return (Math.abs(diff) + 5) / 3 + winner;
   if (diff <= 2) return winner + 2;
   if (diff < 20) return winner + 1;
   return winner;
@@ -30,5 +33,5 @@ function showMessage(number) {
   return `Рейтинг переможця ${number.toFixed(1)}`;
 }
 
-const newRate = calculateNewRate(13, 6);
+const newRate = calculateNewRate('258', 857);
 console.log(showMessage(newRate));
